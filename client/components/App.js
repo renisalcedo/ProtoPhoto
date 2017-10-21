10dcc23756f9af251201597e73308b8048e791e6
Winnie Tam <wtwinnietam@gmail.com>
2017-10-21 17:38:41 -0400

    added component

--- client/components/App.js
+++ client/components/App.js
@@ -3,30 +3,32 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-    <div>
-    	<div class="navbar-fixed">
-	    <nav>
-		    <div class="nav-wrapper navbar-fixed">
-		      <a href="#" class="brand-logo center">Logo</a>
-		      <ul class="left hide-on-med-and-down">
-		        <li><a href="#">Sass</a></li>
-		        <li><a href="#">Components</a></li>
-		        <li class="active"><a href="collapsible.html">JavaScript</a></li>
-		      </ul>
-		    </div>
-	  	</nav>
+
+    <div style={{backgroundColor: "#6bcbff"}}>
+    	<div className="navbar-fixed">
+		    <nav className="orange z-depth-5">
+			    <div className="nav-wrapper container">
+			      <a href="#" className="brand-logo center">Logo</a>
+			      <ul className="left hide-on-med-and-down">
+			        <li><a href="#" className="waves-effect waves-light pulse btn"><i className="material-icons left">home</i>Home</a></li>
+			        <li><a href="#" className="waves-effect waves-light pulse btn"><i className="material-icons left">info</i>About</a></li>
+			      </ul>
+			    </div>
+		  	</nav>
 	  	</div>
-      <h1 className='blue-text text-darken-2'>ProtoPhoto fxaaaaaaaaaafffffffffffffgggggggggggggggggggggggg
-      ggggggggggggggggggggggggggggggggggggggggggggggg
-      hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-      jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-      rrrrrrrrrrrrrrrrrrrrrrrrrrr
-      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-      qqqqqqqqqqqqqqqqqqq
-      oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-      oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-      oooooooooooooooooooooooo</h1>
-      <input type='file' />
+
+	      <h1 className='blue-text text-darken-2'>ProtoPhoto fxaaaaaaaaaafffffffffffffgggggggggggggggggggggggg
+	      ggggggggggggggggggggggggggggggggggggggggggggggg
+	      hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+	      jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+	      rrrrrrrrrrrrrrrrrrrrrrrrrrr
+	      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+	      qqqqqqqqqqqqqqqqqqq
+	      oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+	      oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+	      oooooooooooooooooooooooo</h1>
+
+	      <input type='file' />
     </div>
     )
   }
