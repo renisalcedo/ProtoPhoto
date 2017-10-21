aa4866d8a22d0782ba10adaca16305dac2b2a532
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-10-20 23:59:07 -0400

    Added sample react and dependencies

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1 className='blue-text text-darken-2'>Hello, <br /> pls put picture of sexy web page thank u</h1>
+    )
+  }
+};
+
+export default App;
