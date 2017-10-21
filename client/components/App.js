665e11809e10b754a29fc1e54c1b2f641e62370f
Winnie Tam <wtwinnietam@gmail.com>
2017-10-21 13:59:51 -0400

    Added button and navbar

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <h1 className='blue-text text-darken-2'>Hello, <br /> pls put picture of sexy web page thank u</h1>
+    <div>
+    	<div class="navbar-fixed">
+	    <nav>
+		    <div class="nav-wrapper navbar-fixed">
+		      <a href="#" class="brand-logo center">Logo</a>
+		      <ul class="left hide-on-med-and-down">
+		        <li><a href="#">Sass</a></li>
+		        <li><a href="#">Components</a></li>
+		        <li class="active"><a href="collapsible.html">JavaScript</a></li>
+		      </ul>
+		    </div>
+	  	</nav>
+	  	</div>
+      <h1 className='blue-text text-darken-2'>ProtoPhoto fxaaaaaaaaaafffffffffffffgggggggggggggggggggggggg
+      ggggggggggggggggggggggggggggggggggggggggggggggg
+      hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+      jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+      rrrrrrrrrrrrrrrrrrrrrrrrrrr
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      qqqqqqqqqqqqqqqqqqq
+      oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+      oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+      oooooooooooooooooooooooo</h1>
+      <input type='file' />
+    </div>
     )
   }
 };
