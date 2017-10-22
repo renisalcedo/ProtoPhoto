c9531e1eba0e6d30c919fb585c451f01213d0b50
ASrima <ayeshasultanarima@gmail.com>
2017-10-21 23:48:38 -0400

    made changes to the footer file

--- client/components/footer.js
+++ client/components/footer.js
@@ -5,23 +5,17 @@ class Footer extends Component {
 		return(
 
 <div class="container">
-    <footer>
+ <footer>
   <nav>
    <section>
-    <h1>Website Generator</h1>
-    <p><img src="images/somersaults.jpeg" alt=""/></p>
-    <p><img src="images/kindplus.jpeg"/></p>
-    <p><img src="images/crisps.jpeg"/> 
+    	<h1>Website Generator</h1>
+    	
    </section>
-   <ul>
-    <li> <a href="/about">About us...</a>
-    <li> <a href="/feedback">Send feedback!</a>
-    <li> <a href="/sitemap">Sitemap</a>
-   </ul>
+   
   </nav>
-  <p><small>Powered with ♥  Google ML and Microsoft Azure
+  <p>Powered with ♥  Google ML and Microsoft Azure </p>
  </footer>
-</body>
+
     </div>
     )}
 	};
